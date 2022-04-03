e329437b50708bc8a2643b8c5c732167102e7dc7
cannot <vitaliydch@gmail.com>
2022-04-03 17:46:21 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	http.HandleFunc("/ha", func(writer http.ResponseWriter, request *http.Request) {
+		log, err := os.OpenFile("logs.log", os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			panic(err)
+		}
+		str := fmt.Sprintf("%s :: %s \n", "new request", time.Now())
+		_, err = log.WriteString(str)
+		if err != nil {
+			panic(err)
+			return
+		}
+
+		fmt.Println("new request", time.Now())
+
+	})
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		panic(err)
+		return
+	}
+}
