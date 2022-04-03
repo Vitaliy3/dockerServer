882279e1e7bb0da19f34c4774507db88ab4cbdfd
cannot <vitaliydch@gmail.com>
2022-04-04 03:13:15 +0400

    logs to stdout, add param for send data

--- main.go
+++ main.go
@@ -1,29 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
 
-	http.HandleFunc("/ha", func(writer http.ResponseWriter, request *http.Request) {
-		log, err := os.OpenFile("logs.log",os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			panic(err)
-		}
-		str := fmt.Sprintf("%s :: %s \n", "new request", time.Now())
-		_, err = log.WriteString(str)
-		if err != nil {
-			panic(err)
+	http.HandleFunc("/data", func(writer http.ResponseWriter, request *http.Request) {
+
+		keys, ok := request.URL.Query()["text"]
+
+		if !ok || len(keys[0]) < 1 {
+			log.Println("Url Param 'text' is missing")
 			return
 		}
 
-		fmt.Println("new request", time.Now())
-
+		log.Printf("%s %s \n", time.Now().Format(time.RFC850), keys[0])
 	})
+
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		panic(err)
